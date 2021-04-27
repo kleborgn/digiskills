8e8ee437e1dba34e2e4bb22b206be17dbd8a6945
evan <evan.scoarnec@etudiant.univ-rennes1.fr>
2021-04-27 14:59:15 +0200

    Quiz

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
+import Start from './components/Start';
+import Question from './components/Question';
+import quizData from './data/quiz.json';
+import End from './components/End';
+import Modal from './components/Modal';
 const App = () => {
+  const [step, setStep] = useState(1);
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [answers, setAnswers] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
+  const quizStartHandler = () => {
+    setStep(2);
+  }
+
+  const resetClickHandler = () => {
+    setActiveQuestion(0);
+    setAnswers([]);
+    setStep(2);
+  }
   return (
     <div className="App" >
+      {step === 1 && <Start onQuizStart={quizStartHandler} />}
+      {step === 2 && <Question
+        data={quizData.data[activeQuestion]}
+        onAnswerUpdate={setAnswers}
+        numberOfQuestions={quizData.data.length}
+        activeQuestion={activeQuestion}
+        onSetActiveQuestion={setActiveQuestion}
+        onSetStep={setStep}
+      />}
+      {step === 3 && <End
+        results={answers}
+        data={quizData.data}
+        onReset={resetClickHandler}
+        onAnswersCheck={() => { setShowModal(true)}}
+      />}
+      {showModal && <Modal 
+      onClose={() => setShowModal(false)}
+      results={answers}
+      data={quizData.data}
+      />}
     </div>
   );
 }
