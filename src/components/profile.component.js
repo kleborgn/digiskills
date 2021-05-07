2d501a31e7ae23b0f3693841e7609b61c2f32ccc
evan <evan.scoarnec@etudiant.univ-rennes1.fr>
2021-05-07 22:00:41 +0200

    css Profil

--- src/components/profile.component.js
+++ src/components/profile.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import AuthService from "../services/auth.service";
+import './profil.css';
 
 export default class Profile extends Component {
     constructor(props) {
@@ -14,30 +15,33 @@ export default class Profile extends Component {
         const { currentUser } = this.state;
 
         return (
-            <div className="container">
-                <header className="jumbotron">
+            <div className="profil-container">
+                <div className="profil-container-right">
                     <h3>
-                        <strong>{currentUser.username}</strong> Profile
+                        <strong>Profile: </strong>{currentUser.username}
                     </h3>
-                </header>
-                <p>
+                
+                <p className="profil-content">
                     <strong>Token:</strong>{" "}
                     {currentUser.accessToken.substring(0, 20)} ...{" "}
                     {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
                 </p>
-                <p>
+                <p className="profil-content">
                     <strong>Id:</strong>{" "}
                     {currentUser.id}
                 </p>
-                <p>
+                <p className="profil-content">
                     <strong>Email:</strong>{" "}
                     {currentUser.email}
                 </p>
+                <p className="profil-content">
                 <strong>Authorities:</strong>
-                <ul>
+                <ul className="profil-content">
                     {currentUser.roles &&
                     currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
                 </ul>
+                </p>
+                </div>
             </div>
         );
     }
