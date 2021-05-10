9e7bba8049dfb93d442486cbf7e885733bc47f4a
evan <evan.scoarnec@etudiant.univ-rennes1.fr>
2021-05-10 14:11:32 +0200

    add usefull font

--- src/components/pages/Tools.js
+++ src/components/pages/Tools.js
@@ -23,7 +23,7 @@ const Tools = () => {
             </div>
             <div className="container__tools">
             <h1>Gimp</h1>
-                        <img src="/images/discord.svg" alt="discord" />
+                        <img src="/images/gimp.svg" alt="discord" />
                     <div className="content__tools">
                     
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus ullam, quisquam nemo iste error minus eveniet quis commodi autem incidunt aut nulla itaque aperiam iure? Quos ab aut quaerat quis eaque rem optio culpa fuga tempore totam ea sit voluptatum dolore eum placeat, maxime excepturi distinctio ad recusandae. Adipisci illo porro nam atque accusamus possimus mollitia nulla quo a incidunt nesciunt, molestiae ipsum eius cumque nisi cupiditate sit eaque laborum quisquam laboriosam saepe! Culpa rem expedita quaerat alias incidunt rerum ipsam enim hic veniam necessitatibus, minima quo sapiente, eum a fugiat autem voluptatem aperiam vitae adipisci quia molestiae. Facilis rem similique eaque quas est rerum aperiam in optio id eveniet vitae dolorem explicabo, possimus tenetur, sapiente mollitia. Modi, sunt quibusdam odio, commodi eius magni voluptates quas, alias repellat asperiores assumenda hic totam obcaecati temporibus cumque deserunt ex nobis? Ipsa illo magni a neque asperiores commodi atque sit esse consequuntur!</p>
@@ -32,7 +32,7 @@ const Tools = () => {
             </div>
             <div className="container__tools">
             <h1>Teams</h1>
-                        <img src="/images/discord.svg" alt="discord" />
+                        <img src="/images/Teams.svg" alt="discord" />
                     <div className="content__tools">
                     
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus ullam, quisquam nemo iste error minus eveniet quis commodi autem incidunt aut nulla itaque aperiam iure? Quos ab aut quaerat quis eaque rem optio culpa fuga tempore totam ea sit voluptatum dolore eum placeat, maxime excepturi distinctio ad recusandae. Adipisci illo porro nam atque accusamus possimus mollitia nulla quo a incidunt nesciunt, molestiae ipsum eius cumque nisi cupiditate sit eaque laborum quisquam laboriosam saepe! Culpa rem expedita quaerat alias incidunt rerum ipsam enim hic veniam necessitatibus, minima quo sapiente, eum a fugiat autem voluptatem aperiam vitae adipisci quia molestiae. Facilis rem similique eaque quas est rerum aperiam in optio id eveniet vitae dolorem explicabo, possimus tenetur, sapiente mollitia. Modi, sunt quibusdam odio, commodi eius magni voluptates quas, alias repellat asperiores assumenda hic totam obcaecati temporibus cumque deserunt ex nobis? Ipsa illo magni a neque asperiores commodi atque sit esse consequuntur!</p>
@@ -41,7 +41,34 @@ const Tools = () => {
             </div>
             <div className="container__tools">
             <h1>Google Doc</h1>
-                        <img src="/images/discord.svg" alt="discord" />
+                        <img src="/images/googledoc.svg" alt="discord" />
+                    <div className="content__tools">
+                    
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus ullam, quisquam nemo iste error minus eveniet quis commodi autem incidunt aut nulla itaque aperiam iure? Quos ab aut quaerat quis eaque rem optio culpa fuga tempore totam ea sit voluptatum dolore eum placeat, maxime excepturi distinctio ad recusandae. Adipisci illo porro nam atque accusamus possimus mollitia nulla quo a incidunt nesciunt, molestiae ipsum eius cumque nisi cupiditate sit eaque laborum quisquam laboriosam saepe! Culpa rem expedita quaerat alias incidunt rerum ipsam enim hic veniam necessitatibus, minima quo sapiente, eum a fugiat autem voluptatem aperiam vitae adipisci quia molestiae. Facilis rem similique eaque quas est rerum aperiam in optio id eveniet vitae dolorem explicabo, possimus tenetur, sapiente mollitia. Modi, sunt quibusdam odio, commodi eius magni voluptates quas, alias repellat asperiores assumenda hic totam obcaecati temporibus cumque deserunt ex nobis? Ipsa illo magni a neque asperiores commodi atque sit esse consequuntur!</p>
+                    </div>
+                
+            </div>
+            <div className="container__tools">
+            <h1>Winrar</h1>
+                        <img src="/images/winrar.svg" alt="discord" />
+                    <div className="content__tools">
+                    
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus ullam, quisquam nemo iste error minus eveniet quis commodi autem incidunt aut nulla itaque aperiam iure? Quos ab aut quaerat quis eaque rem optio culpa fuga tempore totam ea sit voluptatum dolore eum placeat, maxime excepturi distinctio ad recusandae. Adipisci illo porro nam atque accusamus possimus mollitia nulla quo a incidunt nesciunt, molestiae ipsum eius cumque nisi cupiditate sit eaque laborum quisquam laboriosam saepe! Culpa rem expedita quaerat alias incidunt rerum ipsam enim hic veniam necessitatibus, minima quo sapiente, eum a fugiat autem voluptatem aperiam vitae adipisci quia molestiae. Facilis rem similique eaque quas est rerum aperiam in optio id eveniet vitae dolorem explicabo, possimus tenetur, sapiente mollitia. Modi, sunt quibusdam odio, commodi eius magni voluptates quas, alias repellat asperiores assumenda hic totam obcaecati temporibus cumque deserunt ex nobis? Ipsa illo magni a neque asperiores commodi atque sit esse consequuntur!</p>
+                    </div>
+                
+            </div>
+            <div className="container__tools">
+            <h1>Trello</h1>
+                        <img src="/images/trello.svg" alt="discord" />
+                    <div className="content__tools">
+                    
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus ullam, quisquam nemo iste error minus eveniet quis commodi autem incidunt aut nulla itaque aperiam iure? Quos ab aut quaerat quis eaque rem optio culpa fuga tempore totam ea sit voluptatum dolore eum placeat, maxime excepturi distinctio ad recusandae. Adipisci illo porro nam atque accusamus possimus mollitia nulla quo a incidunt nesciunt, molestiae ipsum eius cumque nisi cupiditate sit eaque laborum quisquam laboriosam saepe! Culpa rem expedita quaerat alias incidunt rerum ipsam enim hic veniam necessitatibus, minima quo sapiente, eum a fugiat autem voluptatem aperiam vitae adipisci quia molestiae. Facilis rem similique eaque quas est rerum aperiam in optio id eveniet vitae dolorem explicabo, possimus tenetur, sapiente mollitia. Modi, sunt quibusdam odio, commodi eius magni voluptates quas, alias repellat asperiores assumenda hic totam obcaecati temporibus cumque deserunt ex nobis? Ipsa illo magni a neque asperiores commodi atque sit esse consequuntur!</p>
+                    </div>
+                
+            </div>
+            <div className="container__tools">
+            <h1>Office</h1>
+                        <img src="/images/office.svg" alt="discord" />
                     <div className="content__tools">
                     
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus ullam, quisquam nemo iste error minus eveniet quis commodi autem incidunt aut nulla itaque aperiam iure? Quos ab aut quaerat quis eaque rem optio culpa fuga tempore totam ea sit voluptatum dolore eum placeat, maxime excepturi distinctio ad recusandae. Adipisci illo porro nam atque accusamus possimus mollitia nulla quo a incidunt nesciunt, molestiae ipsum eius cumque nisi cupiditate sit eaque laborum quisquam laboriosam saepe! Culpa rem expedita quaerat alias incidunt rerum ipsam enim hic veniam necessitatibus, minima quo sapiente, eum a fugiat autem voluptatem aperiam vitae adipisci quia molestiae. Facilis rem similique eaque quas est rerum aperiam in optio id eveniet vitae dolorem explicabo, possimus tenetur, sapiente mollitia. Modi, sunt quibusdam odio, commodi eius magni voluptates quas, alias repellat asperiores assumenda hic totam obcaecati temporibus cumque deserunt ex nobis? Ipsa illo magni a neque asperiores commodi atque sit esse consequuntur!</p>
