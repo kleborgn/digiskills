cb3876759d12da536c54543155705ac2e5d08694
evan <evan.scoarnec@etudiant.univ-rennes1.fr>
2021-05-27 10:31:47 +0200

    404Page done

---
+++ src/components/pages/Page404.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../css/Page404.css';
+const Page404 = () => {
+    return (
+        <div>
+            <div className="start">
+            <div class="central-body">
+                <img class="image-404" src="http://salehriaz.com/404Page/img/404.svg" width="300px"/>
+            </div>
+            <div class="objects">
+                <img class="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px"/>
+                <div class="earth-moon">
+                    <img class="object_earth" src="http://salehriaz.com/404Page/img/earth.svg" width="100px"/>
+                    <img class="object_moon" src="http://salehriaz.com/404Page/img/moon.svg" width="80px"/>
+                </div>
+            </div>
+            <div class="glowing_stars">
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Page404
