daf75f245d98b07bfd5df0dc0ff631b84491821c
evan <evan.scoarnec@etudiant.univ-rennes1.fr>
2021-05-31 18:07:14 +0200

    Profil

--- src/components/profile.component.js
+++ src/components/profile.component.js
@@ -15,30 +15,28 @@ export default class Profile extends Component {
     render() {
         const { currentUser } = this.state;
 
-        return ( <
-            div className = "profil-container" >
-            <
-            div className = "profil-container-right" >
-            <
-            h3 >
-            <
-            strong > Profile: < /strong>{currentUser.username} <
-            /h3> <
-            p className = "profil-content" >
-            <
-            strong > Id: < /strong>{" "} { currentUser.id } <
-            /p> <
-            p className = "profil-content" >
-            <
-            strong > Email: < /strong>{" "} { currentUser.email } <
-            /p> <
-            button onClick = { AuthService.logout } > < a href = "./" > LOGOUT < /a></button >
-            <
-            /div>
+        return (
+            <div className="profil-container">
+                <div className="profil-container-right">
+                    <h3>
+                        <strong>Profile: </strong>{currentUser.username}
+                    </h3>
+                    <p className="profil-content">
+                        <strong>Id:</strong>{" "}
+                        {currentUser.id}
+                    </p>
+                    <p className="profil-content">
+                        <strong>Email:</strong>{" "}
+                        {currentUser.email}
+                    </p>
+                    <p className="profil-content">
+                        <Score></Score>
+                    </p>
+                    <button onClick={AuthService.logout}><a href="./">LOGOUT</a></button>
+                </div>
 
-            <
-            Score > < /Score> <
-            /div>
+                
+            </div>
         );
     }
 }
