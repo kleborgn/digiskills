c6601f720e0340c06eea9b1eae9b0e4572a0ec55
evan <evan.scoarnec@etudiant.univ-rennes1.fr>
2021-06-14 13:43:08 +0200

    ./

--- src/components/Home/HeroSection.js
+++ src/components/Home/HeroSection.js
@@ -4,17 +4,25 @@ import '../css/HeroSection.css';
 
 
 function HeroSection() {
-    return (
-        <div className='hero-container'>
-            <img className="svg-header"src="/images/SvgHeader.svg" alt="/"/>
-            <h1>Welcome to DigiSkills</h1>
-            <p>A WebApp to develop your digital skills</p>
-            <div className="hero-btns">
-                <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>SIGN UP</Button>
-            </div>
+    return ( <
+        div className = 'hero-container' >
+        <
+        img className = "svg-header"
+        src = "./images/SvgHeader.svg"
+        alt = "/" / >
+        <
+        h1 > Welcome to DigiSkills < /h1> <
+        p > A WebApp to develop your digital skills < /p> <
+        div className = "hero-btns" >
+        <
+        Button className = 'btns'
+        buttonStyle = 'btn--outline'
+        buttonSize = 'btn--large' > SIGN UP < /Button> <
+        /div>
 
-            
-        </div>
+
+        <
+        /div>
     )
 }
 
